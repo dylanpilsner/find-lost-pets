f9716dedfcd2693abe652d0f152445e6f81b5396
unknown <dylan.pilsner@gmail.com>
2022-12-04 22:01:35 -0300

    cambio api_base_url

--- fe-src/state.ts
+++ fe-src/state.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3000";
+const API_BASE_URL = "https://find-lost-pets.onrender.com";
 const state = {
   data: {
     geolocation: {
